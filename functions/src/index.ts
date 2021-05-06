8d2cb58ec9b6dc42f85f80f84acd664ff430ccce
sotaro24oka <nishioka.sotaro@gmail.com>
2021-05-07 00:54:21 +0900

    zantei

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions'
-import buildTokenMap from './util/buildToken'
+// import buildTokenMap from './util/buildToken'
+// import { JSDOM } from "jsdom";
 
 exports.updateUrlToUpper = functions
 .region('asia-northeast1')
@@ -10,5 +11,21 @@ exports.updateUrlToUpper = functions
   const url = data.url as string;
 
   const res = await fetch(url);
-  const html = await res.text();
+  const html = await res.text();// 多分JSDOMの引数部分から間違っている
+  // const dom = new JSDOM(html);
+  // const metas = dom.window.document.querySelectorAll("head > meta");
+  // const metaArrays = Array.from(metas)
+  // const test = metaArrays[0].getAttribute("content")
+  // const metaValues = metaArrays.reduce((pre, ogp) => {
+  //   const property = ogp.getAttribute("property").trim().replace("og:", "");
+  //   if(property==='site_name'){
+
+  //   }
+  //   const content = ogp.getAttribute("content");
+  //   pre[property] = content;
+  //   return pre;
+  // }, {});
+  return snap.ref.set({
+    bar: html
+  }, {merge: true});
 })
