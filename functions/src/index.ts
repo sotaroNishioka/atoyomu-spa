26e9e5a0061a444ba062ff07708e6fc1a5fffd68
sotaroNishioka <nishiokanandesuka@gmail.com>
2022-06-06 01:10:11 +0900

    Revert "func にLintを追加"
    This reverts commit 327b4363e5b56d1cf8a4590245d0b42eaf75e36a.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,45 @@
 import * as functions from 'firebase-functions'
+import * as osmosis from 'osmosis'
 import buildTokenMap from './util/buildToken'
 
 exports.updateUrlToUpper = functions
 .region('asia-northeast1')
 .firestore
 .document('readingLists/{docId}')
-.onCreate(async(snap, context) => {
-  const data = snap.data(); 
+.onCreate((snap, context) => {
+  const data = snap.data();
   const url = data.url as string;
 
-  const res = await fetch(url);
-  const html = await res.text();
+  osmosis
+  .get(url)
+  .find('head')
+  .set({
+    site_name: "meta[property='og:site_name']@content",
+    url: "meta[property='og:url']@content",
+    title: "meta[property='og:title']@content",
+    type: "meta[property='og:type']@content",
+    description: "meta[property='og:description']@content",
+    image: "meta[property='og:image']@content"
+  })
+  .data(({site_name,url,title,type,description,image}) => {
+    functions.logger.info({site_name,url,title,type,description,image})
+    const tokenMap = buildTokenMap(`${title} ${description}`);
+    return snap.ref.set({
+      site_name_new: site_name,
+      url_new: url,
+      title_new: title,
+      type_new: type,
+      description_new: description,
+      image_new: image,
+      tokenMap
+    }, {merge: true});
+  })
+  .error(err => {
+    functions.logger.error(url)
+    functions.logger.error('osmosisでエラー')
+    functions.logger.error(err)
+    return snap.ref.set({
+      isError: true
+    }, {merge: true});
+  })
 })
